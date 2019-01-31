10f049c4d5566111fb352eeed7897252928e9fa6
dShvetsov <rbbozk@yandex.ru>
2019-01-31 22:40:28 +0300

    Additional applications

---
+++ src/DropAll.cc
@@ -0,0 +1,22 @@
+#pragma once
+#include "Application.hh"
+#include "Loader.hh"
+#include "Maple.hh"
+#include "Decision.hh"
+#include "api/Packet.hh"
+
+class DropAll : public Application {
+SIMPLE_APPLICATION(DropAll, "drop-all")
+public:
+    void init(Loader* loader, const Config& config) override
+    {
+        auto maple = Maple::get(loader);
+        maple->registerHandler("drop-all",
+            [=](Packet& pkt, FlowPtr, Decision decision) {
+                return decision.drop();
+            }
+        );
+    }
+};
+
+REGISTER_APPLICATION(DropAll, {"maple", ""})
