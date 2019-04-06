f2e35c0f11694386718447f681203746ee932ad1
denshv <rbbozk@yandex.ru>
2019-04-06 16:54:19 +0300

    Log to file also

--- debug_run_env.sh
+++ debug_run_env.sh
@@ -1,5 +1,5 @@
 # Print glog messages
 export DYLD_LIBRARY_PATH=$PWD/prefix/lib:$DYLD_LIBRARY_PATH # OS X
 export LD_LIBRARY_PATH=$PWD/prefix/lib:$LD_LIBRARY_PATH   # Linux
-export GLOG_logtostderr=1
+export GLOG_alsologtostderr=1
 export GLOG_colorlogtostderr=1
